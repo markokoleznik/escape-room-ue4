518477a2ec74f7050e7bc3987606379c066b1321
Marko Koleznik <marko@koleznik.net>
2017-01-07 16:30:57 +0100

    Added OpenDoor Component.

---
+++ Source/EscapeRoom/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EscapeRoom.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+    OpenDoor();
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
+void UOpenDoor::OpenDoor()
+{
+    
+    AActor* Owner = GetOwner();
+    
+    FRotator Rotator = FRotator(0.0f, OpenAngle, 0.0f);
+    
+    Owner->SetActorRotation(Rotator);
+}
+
