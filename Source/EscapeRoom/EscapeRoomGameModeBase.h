a3d12a2fe238bf7547df191d0f6b81fe10f05d33
Marko Koleznik <marko@koleznik.net>
2017-01-07 15:38:51 +0100

    Empty Project, skeleton.

---
+++ Source/EscapeRoom/EscapeRoomGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "EscapeRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOM_API AEscapeRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
